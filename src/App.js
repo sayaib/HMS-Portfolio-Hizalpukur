eb419ee16cfa526befc363db0f807a197c274411
Sayaib Sarkar <66420972+sayaib@users.noreply.github.com>
2023-01-08 14:57:32 +0530

    uploaded first code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import IntroSection from './component/IntroSection'
+
+function App() {
+  return (
+    <div className="App">
+      <IntroSection />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/IntroSection.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../css/IntroSection.css";
+
+const IntroSection = () => {
+  return (
+    <>
+      <div>This is intro</div>
+    </>
+  );
+};
+
+export default IntroSection;
