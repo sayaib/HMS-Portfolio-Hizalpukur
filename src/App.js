494bd6abeebbba24878158bae1f90646707afd22
Sayaib Sarkar <66420972+sayaib@users.noreply.github.com>
2023-01-08 16:34:40 +0530

    stage one

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import IntroSection from './component/IntroSection'
+import AboutUs from './component/AboutUs';
+import Management from './component/Management';
+import GuidingPrinciples from './component/GudingPrinciples';
 
 function App() {
   return (
     <div className="App">
       <IntroSection />
+      <AboutUs />
+      <Management />
+      <GuidingPrinciples />
     </div>
   );
 }
